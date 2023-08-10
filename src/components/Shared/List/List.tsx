fdd06563557db331c403414a2b4c74a3b0b80ec0
MahmoudZaqoutt <mahmoudzaqoutt12333@gmail.com>
2023-08-10 15:34:27 +0300

    tset  pages

--- src/components/Shared/List/List.tsx
+++ src/components/Shared/List/List.tsx
@@ -1,20 +1,19 @@
 import { IPropsTopHeaderList } from "@/interfaces/props/IPropsTopHeaderList";
 import Link from "next/link";
-import React from "react";
-
+import React, { Fragment } from "react";
 export const List = (props: IPropsTopHeaderList) => {
   return (
     <ul className={props.className}>
-      {props.options.map((option, index) => (
-        <>
+      {props.options.map((option: any, index) => (
+        <Fragment key={index}>
           {option.modal === "modal" ? (
             <li>{option.content}</li>
           ) : (
-            <Link href={option.href} key={index}>
+            <Link href={option.href}>
               <li className={option.classNameOfLI}>{option.content}</li>
             </Link>
           )}
-        </>
+        </Fragment>
       ))}
     </ul>
   );
