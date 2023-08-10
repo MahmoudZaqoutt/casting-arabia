7703792c1fe063c4e30883d19b0a80b456baf638
MahmoudZaqoutt <mahmoudzaqoutt12333@gmail.com>
2023-08-10 15:05:14 +0300

    create register  pages

---
+++ src/pages/SeekerRegister/index.tsx
@@ -0,0 +1,12 @@
+import SeekerRegister from "@/components/SeekerRegister/SeekerRegister";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <SeekerRegister />
+    </div>
+  );
+};
+
+export default index;
---
+++ src/pages/TalentRegister/index.tsx
@@ -0,0 +1,8 @@
+import TalentRegister from "@/components/TalentRegister/TalentRegister";
+import React from "react";
+
+const index = () => {
+  return <TalentRegister />;
+};
+
+export default index;
